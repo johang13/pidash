e0937473f31ddd39d5b2ea397821d0766cd5f886
johang13 <chris.grobler@outlook.co.nz>
2026-04-21 16:06:45 +1200

    Address pylint errors

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def print_hi(name):
-    print(f'Hi, {name}')
+"""Minimal entrypoint script for printing a greeting."""
 
-if __name__ == '__main__':
-    print_hi('PyCharm')
+
+def print_hi(name):
+    """Print a friendly greeting for the provided name."""
+    print(f"Hi, {name}")
+
+
+if __name__ == "__main__":
+    print_hi("PyCharm")
